ef6dbaa8e141c327243cc9a37d991140844f197b
1319105206@qq.com <jyxyz123456789>
2020-08-22 22:37:59 +0800

    jdk源码初始化提交

---
+++ JavaSourceLern/src/test/hashmap/Test.java
@@ -0,0 +1,12 @@
+package test.hashmap;
+
+import java.util.HashMap;
+
+public class Test {
+
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+        hashMap.put("11", 11);
+        System.out.println("hashMap = " + hashMap);
+    }
+}
